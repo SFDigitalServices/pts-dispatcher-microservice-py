47df2b726adf9ed82eaaae939a7f10c3fa8ffc67
hshaosf <11635576+hshaosf@users.noreply.github.com>
2020-05-15 00:25:24 -0700

    pts csv export

---
+++ service/resources/export.py
@@ -0,0 +1,122 @@
+"""Welcome export module"""
+import json
+import datetime
+import os
+import base64
+import pytz
+import falcon
+import jsend
+import sendgrid
+import sentry_sdk
+from ..modules.formio import Formio
+from ..transforms.export_submissions import ExportSubmissionsTransform
+from .hooks import validate_access
+
+@falcon.before(validate_access)
+class Export():
+    """Export class"""
+    def on_get(self, req, resp):
+        #pylint: disable=no-self-use,too-many-locals
+        """
+        on get request
+        return export message and response if successful
+        """
+        try:
+            timezone = pytz.timezone('America/Los_Angeles')
+
+            yesterday = datetime.datetime.now(timezone) - datetime.timedelta(days=1)
+            start_datetime_obj = datetime.datetime.combine(
+                yesterday, datetime.datetime.min.time())
+
+            # if start_date provided
+            if 'start_date' in req.params:
+                start_datetime_obj = datetime.datetime.strptime(
+                    req.params['start_date'], '%Y-%m-%d')
+
+            start_time_utc = timezone.localize(start_datetime_obj).astimezone(pytz.UTC)
+
+            # how many days we want included in report starting from start_date
+            report_days = int(req.params['days']) if 'days' in req.params else 1
+
+            end_datetime_obj = start_datetime_obj + datetime.timedelta(days=report_days)
+            end_time_utc = timezone.localize(end_datetime_obj).astimezone(pytz.UTC)
+
+            responses = Formio.get_formio_submission_by_query(
+                {
+                    'created__gte':start_time_utc.isoformat(),
+                    'created__lt':end_time_utc.isoformat(),
+                    'limit':2000*report_days
+                })
+
+            submissions_csv = ExportSubmissionsTransform().transform(responses)
+            msg = 'Export '+str(start_time_utc.isoformat())+' to '+str(end_time_utc.isoformat())
+
+            send_email = bool(req.params['send_email']) if 'send_email' in req.params else False
+            if send_email:
+                self.email(
+                    "Export "+str(start_datetime_obj.date()),
+                    content=msg,
+                    file_name="export-"+str(start_datetime_obj.date())+".csv",
+                    file_content=submissions_csv)
+
+            resp.body = json.dumps(jsend.success({'message': msg, 'responses':len(responses)}))
+            resp.status = falcon.HTTP_200
+
+        #pylint: disable=broad-except
+        except Exception as exception:
+            resp.status = falcon.HTTP_500
+            msg_error = repr(exception)
+            sentry_sdk.capture_message(msg_error)
+            resp.body = json.dumps(jsend.error(msg_error))
+
+    def email(self, subject, content="Hi", file_name=None, file_content=None):
+        """ Email CSV """
+        #pylint: disable=too-many-locals
+        from_email = sendgrid.helpers.mail.Email(os.environ.get('EXPORT_EMAIL_FROM'))
+        to_emails = os.environ.get('EXPORT_EMAIL_TO')
+        cc_emails = os.environ.get('EXPORT_EMAIL_CC', None)
+        bcc_emails = os.environ.get('EXPORT_EMAIL_BCC', None)
+
+        content = sendgrid.helpers.mail.Content("text/plain", content)
+
+        mail = sendgrid.helpers.mail.Mail(
+            from_email=from_email,
+            subject=subject,
+            plain_text_content=content)
+
+        personalization = sendgrid.helpers.mail.Personalization()
+
+        for _idx, val in enumerate(to_emails.split(',')):
+            personalization.add_to(sendgrid.helpers.mail.Email(val))
+
+        if cc_emails:
+            for _idx, val in enumerate(cc_emails.split(',')):
+                personalization.add_cc(sendgrid.helpers.mail.Email(val))
+        if bcc_emails:
+            for _idx, val in enumerate(bcc_emails.split(',')):
+                personalization.add_bcc(sendgrid.helpers.mail.Email(val))
+
+        mail.add_personalization(personalization)
+
+        if file_content:
+            encoded = base64.b64encode(file_content.encode("utf-8")).decode()
+            attachment = sendgrid.helpers.mail.Attachment()
+            attachment.file_content = sendgrid.helpers.mail.FileContent(encoded)
+            attachment.file_type = sendgrid.helpers.mail.FileType('text/csv')
+            attachment.file_name = sendgrid.helpers.mail.FileName(file_name)
+            attachment.disposition = sendgrid.helpers.mail.Disposition('attachment')
+            attachment.content_id = sendgrid.helpers.mail.ContentId(file_name+' Content ID')
+            mail.attachment = attachment
+
+        response = self.send_email(mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+    @staticmethod
+    def send_email(data):
+        """
+        Send email via sendgrid
+        """
+        sg_api = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+        return sg_api.client.mail.send.post(request_body=data)
---
+++ service/transforms/export_submissions.py
@@ -0,0 +1,107 @@
+""" Export Submissions Transform module """
+#pylint: disable=too-few-public-methods
+import dateutil.parser
+import pytz
+import pandas as pd
+from .transform import TransformBase
+
+class ExportSubmissionsTransform(TransformBase):
+    """ Transform for Export Submissions """
+    def transform(self, data):
+        """
+        transform submissions from export
+        """
+        output = list(map(self.get_data, data))
+        output = list(map(self.pretty_format, output))
+        output = self.normalize(output)
+        output = self.to_csv(output)
+        return output
+
+    @staticmethod
+    def get_data(submission):
+        """
+        Get data from submission object
+        """
+        output = {}
+        data = submission['data']
+
+        #pylint: disable=too-many-nested-blocks
+        for key in data:
+            # flatten list values
+            if isinstance(data[key], list):
+                if len(data[key]) > 0:
+                    if isinstance(data[key][0], str):
+                        output[key] = ', '.join(data[key])
+                    else:
+                        file_names = []
+                        for index, val in enumerate(data[key]):
+                            # if storage, concat filename
+                            if 'storage' in val and 'originalName' in val:
+                                file_names.append(val['originalName'])
+                            else:
+                                output[key+str(index+1)] = val
+
+                        if len(file_names) > 0:
+                            output[key] = ', '.join(file_names)
+            else:
+                output[key] = data[key]
+
+        # append id and created field to data
+        output['id'] = submission['_id']
+        output['created'] = submission['created']
+        return output
+
+    def normalize(self, data):
+        """
+        Normalize data into a flat structure into DataFrame
+        """
+        dataframe = pd.json_normalize(data)
+
+        # move id and created to front
+        col = dataframe.pop("created")
+        dataframe.insert(0, col.name, col)
+        col = dataframe.pop("id")
+        dataframe.insert(0, col.name, col)
+
+        # update column names
+        dataframe.rename(columns=self.pretty_string, inplace=True)
+
+        return dataframe
+
+    @staticmethod
+    def to_csv(dataframe):
+        """
+        Return CSV from DataFrame
+        """
+        return dataframe.to_csv(index=False)
+
+    def pretty_format(self, data):
+        """ Pretty format data fields """
+        output = {}
+        for key in data:
+            output[key] = self.pretty_time(data[key])
+        return output
+
+    def pretty_time(self, value, zone='America/Los_Angeles'):
+        """
+        If valid date, return a better human readable time string
+        """
+        new_value = value
+        if self.datetime_valid(value):
+            time = dateutil.parser.parse(value)
+            timezone = pytz.timezone(zone)
+            localtime = time.astimezone(timezone).strftime('%Y-%m-%d %I:%M:%S %p')
+            return localtime
+        return new_value
+
+    @staticmethod
+    def datetime_valid(dt_str):
+        """ Check if string is valid datetime """
+        try:
+            time = dateutil.parser.parse(dt_str)
+            time_str = time.isoformat("T", "milliseconds").replace("+00:00", "Z")
+            return time_str == dt_str
+        #pylint: disable=bare-except
+        except:
+            pass
+        return False
